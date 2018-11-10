d23e2ff3cb6dc8f139444aae47db229ada55aeba
Bruno Miguel Queiros <bqueiros@gmail.com>
2018-11-10 12:17:15 +0000

    test

--- travis-build.sh
+++ travis-build.sh
@@ -14,7 +14,10 @@ apt-get --yes install devscripts lintian build-essential automake autotools-dev 
 mk-build-deps -i -t "apt-get --yes" -r
 
 ### Build Deb
-mkdir source
-mv ./* source/ # Hack for debuild
-cd source
+#mkdir source
+#mv ./* source/ # Hack for debuild
+#cd source
+#debuild -b -uc -us
+cd src
+qmake && make
 debuild -b -uc -us
